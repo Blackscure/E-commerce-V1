fdd9048b56698ba2dcf369818ddc8fd3a61edc02
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-03-01 14:53:46 +0300

    modification maded in user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 
-from utils.choices import SEX
+from utils.choices import SEX, USER_TYPE
 
 # Create your models here.
 class User(AbstractUser):
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
     email = models.CharField(max_length=80, unique=True)
     sex = models.CharField(max_length=42, choices=SEX, default='male')
-    mobile_number = models.CharField(max_length=45, unique=True)
     status = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    user_type = models.CharField(max_length=42, choices=USER_TYPE, default='customer')
+   
 
-
-class Customer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    is_customer = models.BooleanField(default=True)
