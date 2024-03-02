35b1b7f95f231569f21d767bbd1397b81008bdee
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-03-02 14:36:36 +0300

    oder view modified

--- orders/api/views.py
+++ orders/api/views.py
@@ -22,11 +22,10 @@ class OrderListCreateView(APIView):
         return Response({'status': False, 'message': 'Invalid data provided', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def send_invoice_email(self, order):
-        # Implement email sending logic here
-        # You can use Django's send_mail function or a third-party library like Django Email Message
-        # Example:
+        # email sending logic here
+        
         subject = 'Invoice for Your Order'
         message = f'Thank you for your order! Here is the breakdown:\n\n{order}'
-        from_email = 'your@example.com'
+        from_email = 'wekesabuyahi@gmail.com'
         to_email = [order.user.email]
         # send_mail(subject, message, from_email, to_email, fail_silently=False)
